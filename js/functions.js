44f5f27ab9ee666eef1683e135d205e1ce22f8ec
Douglas <z.doug@hotmail.com>
2021-06-02 18:12:59 -0300

    Function downArrow removed of functions.js

--- js/functions.js
+++ js/functions.js
@@ -4,15 +4,3 @@ window.onload = function() {
     scrollY: false,
   });
 }
-
-document.onkeydown = downSection;
-
-function downSection(e) {
-  if (e.key == 'ArrowDown') {
-    var elementHeight = document.querySelector("main").clientHeight;
-    window.scrollTo({
-      top: elementHeight,
-      behavior: 'smooth'
-    });
-  }
-}
