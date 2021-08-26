aab698ceb3612646709ac5944df3481476822c76
Douglas Oliveira <z.doug@hotmail.com>
2021-08-26 13:15:01 -0300

    Objeto DataScripts criado para armazenar scripts de serviços de rastreamento

--- js/functions.js
+++ js/functions.js
@@ -24,12 +24,21 @@ window.onload = function() {
   }  
   
   function loadDataScripts() {
-    // Google Analytics
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+    DataScripts.googleAnalytics(); 
+  }
 
-    gtag('config', 'UA-119233315-1');
+  DataScripts = {
+      googleAnalytics: function() {
+        var imported = document.createElement('script');
+        imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-119233315-1';
+        document.head.appendChild(imported);
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-119233315-1');
+      }
   }
 
 }
