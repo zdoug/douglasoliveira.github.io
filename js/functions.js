846e06b6d72bc331a4d7a6fbd1d088c75cc5c025
Douglas <z.doug@hotmail.com>
2021-08-26 21:59:55 -0300

    Script GA atualizado

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  
   if(!localStorage.getItem('choiceCookieUser')) {
 
     var cookiesBox = document.querySelector(".cookies");
@@ -29,18 +30,9 @@ window.onload = function() {
 
   DataScripts = {
       googleAnalytics: function() {
-        var imported = document.createElement('script');
-        imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-119233315-1';
-        document.head.appendChild(imported);
-        var importedCode = document.createElement('script');
-        importedCode.createTextNode(dataGoogle);
-        document.head.appendChild(importedCode);
-        
-        var dataGoogle = "";
-        dataGoogle += "window.dataLayer = window.dataLayer || [];";
-        dataGoogle += "function gtag(){dataLayer.push(arguments);}";
-        dataGoogle += "gtag('js', new Date());";
-        dataGoogle += "gtag('config', 'UA-119233315-1');"
+        gtag('consent', 'update', {
+          'ad_storage': 'granted'
+        });
       }
   }
 
