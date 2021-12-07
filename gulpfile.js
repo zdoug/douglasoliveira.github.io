59bca968f80e02c31d37989ca9bea1d9e552fa18
Douglas Oliveira <contato@douglasoliveira.cc>
2021-12-06 22:42:17 -0300

    Gulp tasks added

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp    = require("gulp");
+const plumber = require("gulp-plumber");
+const uglify  = require("gulp-uglify");
+const concat  = require("gulp-concat");
+const rename  = require("gulp-rename");
+
+const js_src = "./src/js/*.js";
+const js_dist  = "./js/";
+const js_dist_name = "scripts.js";
+
+gulp.task('scripts', function(){
+    return gulp.src(js_src)
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(concat(js_dist_name))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(js_dist));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(js_src, gulp.series('scripts'));
+});
+
+gulp.task("default", gulp.series('watch'));
+
