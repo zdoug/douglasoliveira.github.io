ed849849aa434e865bd9a1f7bc598d8107496213
Douglas Oliveira <contato@douglasoliveira.cc>
2022-02-18 17:11:31 -0300

    Novas artes no portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const gulp    = require("gulp");
-const plumber = require("gulp-plumber");
-const uglify  = require("gulp-uglify");
-const concat  = require("gulp-concat");
-const rename  = require("gulp-rename");
+const gulp         = require("gulp");
+const plumber      = require("gulp-plumber");
+const uglify       = require("gulp-uglify");
+const concat       = require("gulp-concat");
+const rename       = require("gulp-rename");
 
-const js_src = "./src/js/*.js";
-const js_dist  = "./js/";
+const js_src       = "./src/js/*.js";
+const js_dist      = "./js/";
 const js_dist_name = "scripts.js";
 
 gulp.task('scripts', function(){
